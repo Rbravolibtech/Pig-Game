92f773c02f1a7bb8117c7d6304160565b2b31f10
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-11 11:32:37 -0600

    added more function to roll dice button

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
-use;
-('use strict');
+'use strict';
+
+// selecting elements this code will make your code appear as zero for player one and two
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// starting conditions this will hide your dice from your page so it does not appear
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. CHECK FOR ROLLED 1: IF TRUE, SWITCH TO NEXT PLAYER
+  if (dice !== 1) {
+    //Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // SWITCH TO NEXT PLAYER
+  }
+});
