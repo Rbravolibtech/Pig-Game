c6754610d89fdb95d8517316dff6e745e87cb9a6
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-11 13:16:05 -0600

    color changes when player wins

--- script.js
+++ script.js
@@ -21,31 +21,66 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // // This code makes it so the color will change when one player gets on the dice 1 the next player will go this will highlight which player is currently playing
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  //1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2. Display dice
+    //2. Display dice
 
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3. CHECK FOR ROLLED 1: IF TRUE, SWITCH TO NEXT PLAYER
-  if (dice !== 1) {
-    //Add dice to the current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // SWITCH TO NEXT PLAYER
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    //3. CHECK FOR ROLLED 1: IF TRUE, SWITCH TO NEXT PLAYER
+    if (dice !== 1) {
+      //Add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // SWITCH TO NEXT PLAYER
+      switchPlayer();
+    }
+  }
+});
 
-    // This code makes it so the color will change when one player gets on the dice 1 the next player will go this will highlight which player is currently playing
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Add current score to active player's score
+
+    scores[activePlayer] += currentScore;
+
+    //scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //2. Check if player's score is >= 100
+
+    if (scores[activePlayer] >= 20) {
+      //Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //SWITCH TO THE NEXT PLAYER
+      switchPlayer();
+    }
   }
 });
